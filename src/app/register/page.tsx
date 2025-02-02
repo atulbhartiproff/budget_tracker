5df46c53b771358b1a112219c91a5eade900f4ae
Atul Bharti <146771106+atulbhartiproff@users.noreply.github.com>
2025-02-02 17:18:43 +0530

    trying something

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,7 +1,34 @@
-import { Input } from "../components/input";
+"use client";
+
+import { useState } from "react";
+// import { Input } from "../components/input";
 import Particles from "../components/Particles";
 
 const Login: React.FC = () => {
+  const [formData, setFormData] = useState({ name: "", email: "", pass: "" });
+  const [responseMessage, setResponseMessage] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await fetch("/api/submit-form", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const result = await response.json();
+      setResponseMessage(result.message);
+    } catch (error) {
+      console.error("Error submitting form:", error);
+      setResponseMessage("Failed to submit form");
+    }
+  };
+
   return (
     <>
       <div className="fixed inset-0 bg-black bg-center pointer-events-none -z-10">
@@ -12,10 +39,40 @@ const Login: React.FC = () => {
           <span className="text-white font-overpass">Register</span>
           <span className="text-blue-500">.</span>
         </h1>
-        <div className="space-y-2">
-          <Input type="username" placeholder="Name" />
-          <Input type="email" placeholder="Email" />
-          <Input type="password" placeholder="Password" />
+        <div>
+          <form
+            onSubmit={handleSubmit}
+            className="space-y-2 flex flex-col justify-center items-center"
+          >
+            <input
+              type="text"
+              name="name"
+              placeholder="Name"
+              value={formData.name}
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              value={formData.email}
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={formData.pass}
+              onChange={handleChange}
+            />
+            <button
+              type="submit"
+              className="px-4 py-1 bg-white text-black rounded-md hover:bg-blue-500 transition"
+            >
+              Submit
+            </button>
+          </form>
+          {responseMessage && <p>{responseMessage}</p>}
         </div>
       </div>
     </>
